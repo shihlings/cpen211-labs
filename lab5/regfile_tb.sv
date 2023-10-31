3e881a85cf6a28c72d4fd0a94538f634df53d632
Shih-Ling Shen <114762019+shihlings@users.noreply.github.com>
2023-10-31 15:29:28 -0700

    Test bench template
    Co-authored-by: Henry <henryrov@users.noreply.github.com>

--- lab5/regfile_tb.sv
+++ lab5/regfile_tb.sv
@@ -1,1 +1,9 @@
-module 
+module regfile_tb ();
+    reg [15:0] data_in;
+    reg [2:0] writenum, readnum;
+    reg write, clk;
+    wire [15:0] data_out;
+
+    regfile DUT (data_in,writenum,write,readnum,clk,data_out);
+    
+endmodule
