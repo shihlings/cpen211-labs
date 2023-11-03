8ee17fac9486d9140f388418cd55fd3338820744
Shih-Ling Shen <114762019+shihlings@users.noreply.github.com>
2023-11-03 11:08:38 -0700

    debugging for testbench

--- lab5/shifter.sv
+++ lab5/shifter.sv
@@ -6,8 +6,8 @@ module shifter(in,shift,sout);
    always_comb begin
       case (shift)
          2'b00: sout = in;
-         2'b01: sout = in << 1;
-         2'b10: sout = in >> 1;
+         2'b01: sout = {in[14:0], 1'b0};
+         2'b10: sout = {1'b0, in[15:1]};
          2'b11: sout = {in[15], in[15:1]};
          default: sout = {16{1'bx}};
       endcase
