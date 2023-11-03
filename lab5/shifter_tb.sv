169fcf6b03f4fed6a8872654afb09927abb3ddbb
Henry Rovner <rovnerhenry@gmail.com>
2023-11-02 19:56:07 -0700

    New shifter test case

--- lab5/shifter_tb.sv
+++ lab5/shifter_tb.sv
@@ -36,16 +36,6 @@ module shifter_tb ();
       #1;
 
       in = 16'd1;
-      shift = 2'b01;
-      #1;
-      if (sout != 16'd2) begin
-         $display ("ERROR sout - Expected %b, actual %b", 16'd2, sout);
-         err = 1'b1;
-      end
-
-      #1;
-
-      in = 16'd1;
       shift = 2'b10;
       #1;
       if (sout != 16'd0) begin
@@ -99,6 +89,47 @@ module shifter_tb ();
          err = 1'b1;
          $display ("ERROR sout - Expected %b, actual %b", 16'b1111100001100111, sout);
       end
+
+      // Test all 1s
+      in = {16{1'b1}};
+      shift = 2'b00;
+      #1;
+      if (sout != {16{1'b1}}) begin
+         $display ("ERROR sout - Expected %b, actual %b", {16{1'b1}}, sout);
+         err = 1'b1;
+      end
+
+      #1;
+
+      in = {16{1'b1}};
+      shift = 2'b01;
+      #1;
+      if (sout != {{15{1'b1}}, 1'b0}) begin
+         $display ("ERROR sout - Expected %b, actual %b", {{15{1'b1}}, 1'b0}, sout);
+         err = 1'b1;
+      end
+
+      #1;
+
+      in = {16{1'b1}};
+      shift = 2'b10;
+      #1;
+      if (sout != {1'b0, {15{1'b1}}}) begin
+         $display ("ERROR sout - Expected %b, actual %b", {1'b0, {15{1'b1}}}, sout);
+         err = 1'b1;
+      end
+
+      #1;
+
+      in = {16{1'b1}};
+      shift = 2'b11;
+      #1;
+      if (sout != {16{1'b1}}) begin
+         $display ("ERROR sout - Expected %b, actual %b", {16{1'b1}}, sout);
+         err = 1'b1;
+      end
+
+      #1;
 
       if (err)
         $display("shifter_tb - ERROR(S) FOUND!");
